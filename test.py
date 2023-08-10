00c245ffd79a00ea910775ec1fd9821e874c825c
veronica.martinez <veronica.martinez@colorado.edu>
2023-08-10 09:14:48 -0600

    commit test file to troubleshoot errors deploying app in the cloud

---
+++ test.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+two_subplot_fig = plt.figure(figsize=(6,6))
+plt.subplot(211)
+plt.plot(t1, f(t1), color='tab:blue', marker=',')
+plt.plot(t2, f(t2), color='black', marker='.')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange', linestyle='--', marker='.')
+
+st.pyplot(two_subplot_fig)
